2056c59489ea6c162cb41b3f5df2f4e41c4e873b
Andres Chavarria <adrchb24@gmail.com>
2022-11-13 19:17:09 -0600

    Vista listar opciones

---
+++ Pasteleria/Controllers/OpcionController.cs
@@ -0,0 +1,23 @@
+﻿using Pasteleria.Models.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Pasteleria.Controllers
+{
+    public class OpcionController : Controller
+    {
+        OpcionModel model = new OpcionModel();
+        // GET: Opcion
+        public ActionResult OpcionesLista()
+        {
+            var resultado = model.OpcionesLista();
+            if (resultado != null)
+                return View(resultado);
+            else
+                return View("Error");
+        }
+    }
+}
